1fb3d4283888f63bba62431a71bc15f45f91366b
Dan Ruth <danjruth@gmail.com>
2020-06-29 15:50:45 -0400

    remove required packages from setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,5 @@ from setuptools import setup, find_packages
 
 setup(name='point_bubble_JHTDB',
       packages=find_packages(),
-      install_requires=[
-          'scipy',
-          'matplotlib',
-          'numpy',
-          'pickle',
-          'time',
-          'pyJHTDB'
-          ],
+      install_requires=[],
       )
