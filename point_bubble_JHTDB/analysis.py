correct calculation of forces in analysis.py to match what's done in model.py

point_bubble_JHTDB/analysis.py
@@ -69,7 +69,7 @@ def load_case(d,calc_forces=False):
         res = d
         
     # calculate the forces, in DNS coords initially
-    slip = res['v'] - res['u']
+    slip = res['v'][:-1] - res['u'][1:]
     vort = get_vorticity(res['velgrad'])
     vol = (res['d']/2.)**3 * 4./3 * np.pi
     
@@ -81,10 +81,10 @@ def load_case(d,calc_forces=False):
     lift = []
     for i in [0,1,2]:
         
-        u_times_deldotu = np.sum(res['velgrad'][...,i,:]*res['u'],axis=-1)
-        press.append((1+res['Cm'])* vol * (res['dudt'][:,:,i] + u_times_deldotu[:,...]))        
-        drag.append(-1*res['Cd'] * 0.5 * np.pi * (res['d']/2)**2 * slip[:,...,i] * np.linalg.norm(slip[:,...],axis=-1))        
-        lift.append(-1 * res['Cl'] * np.cross(slip,vort)[:,...,i] * vol)
+        u_times_deldotu = np.sum(res['velgrad'][1:,:,i,:]*res['u'][1:],axis=-1)
+        press.append((1+res['Cm'])* vol * (res['dudt'][1:,:,i] + u_times_deldotu))        
+        drag.append(-1*res['Cd'] * 0.5 * np.pi * (res['d']/2)**2 * slip[...,i] * np.linalg.norm(slip[:,...],axis=-1))        
+        lift.append(-1 * res['Cl'] * np.cross(slip,vort[1:])[...,i] * vol)
         
     # make each arrays, and rotate
     press = np.moveaxis(np.array(press),0,-1)
@@ -99,18 +99,20 @@ def load_case(d,calc_forces=False):
     res['v'] = rot_all(res['v'],res['g_dir'])
     res['u'] = rot_all(res['u'],res['g_dir'])
     res['x'] = rot_all(res['x'],res['g_dir'])
+    res['slip'] = rot_all(slip,res['g_dir'])
+    res['vort'] = rot_all(vort,res['g_dir'])
     
-    # get vorticity in DNS coords then rotate
-    vort_DNS_coords = get_vorticity(res['velgrad'])
-    res['vort'] = rot_all(vort_DNS_coords,res['g_dir'])
-    
-    # calculate the slip velocity
-    res['slip'] = res['v']-res['u']
+    # drop the velgrad since it hasn't been rotated
+    del res['velgrad']
     
     # see which points to consider (after initial transient)
     res['cond'] = res['t']>model.T_int*2
     mean_rise = np.mean(res['v'][:,:,2],axis=1)
     res['cond'] = res['cond'] * (mean_rise>0)
+    
+    # get rid of the final point in time, so everything has the same length (slip already is the right length)
+    for var in ['v','u','x','t',]:
+        res[var] = res[var][:-1]
     
     return res
     
@@ -121,7 +123,6 @@ def load_case(d,calc_forces=False):
 # '''
     
 def get_vorticity(velgrad):
-    vort = np.zeros((len(velgrad),3)) # 
     velgrad_shape = np.shape(velgrad)
     vort_shape = velgrad_shape[:-1]
     vort = np.zeros(vort_shape)

testing/test_integration_offline.py
@@ -12,7 +12,7 @@ import toolkit.comps
 import pandas as pd
 from point_bubble_JHTDB import analysis, model
 
-case_name = r'res_beta0.10_A0.002_Cm0.50_Cl0.25_Cd1.27_pressureTerm1.000_liftTerm1.000'
+case_name = r'res_beta0.50_A0.042_Cm0.50_Cl0.25_Cd0.80_pressureTerm1.000_liftTerm1.000'
 res = analysis.load_case(toolkit.comps.cf('MILES_C')+r'\\'+r'200717_pointbubble_data\\'+case_name+r'.pkl')
 
 accel = np.moveaxis(np.diff(res['v'],axis=0),0,-1) / np.diff(res['t'])
@@ -23,4 +23,4 @@ grav[:,:,2] = res['grav_z']
 sum_of_forces = res['press']+res['drag']+res['lift']+grav
 
 fig,ax = plt.subplots()
-ax.scatter(accel[:,:,2].flatten(),sum_of_forces[1:,:,2].flatten())
+ax.scatter(accel[:,:,2].flatten(),sum_of_forces[:-1,:,2].flatten())