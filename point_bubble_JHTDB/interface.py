f6af4bd9d33b44efc3b4f5ff3dbc569388bfbda8
Dan on SciServer <danjruth@gmail.com>
2020-12-11 23:13:14 +0000

    changes to interface and model

--- point_bubble_JHTDB/interface.py
+++ point_bubble_JHTDB/interface.py
@@ -16,6 +16,7 @@ except ImportError:
     print('Unable to import pyJHTDB and initialize')
     lJHTDB_available = False
 
+dx = 2*np.pi / 1024.
 
 if lJHTDB_available:
 
@@ -24,10 +25,41 @@ if lJHTDB_available:
         #print(x.astype(np.float32))
         return lJHTDB.getData(t, point_coords=x.copy().astype(np.float32), data_set='isotropic1024coarse', getFunction='getVelocity', sinterp='Lag4', tinterp='PCHIPInt')
     
+    def get_velocity_filtered(t,x,filter_width,lJHTDB=lJHTDB):
+        #print(x.astype(np.float32))
+        return lJHTDB.getBoxFilter(t, point_coords=x.copy().astype(np.float32), data_set='isotropic1024coarse', field='velocity', filter_width=filter_width)
+    
+#     def test_velocity_filtered():
+#         x = np.array([2,3.2,1.9])
+#         t = 4.2
+#         print('a')
+#         a = get_velocity_filtered(t,x.copy(),0.1)
+#         print(a)
+#         a = get_velocity_filtered(t,x.copy(),0.01)
+#         print(a)
+#         a = get_velocity(t,x.copy())
+#         print(a)
+#         return None
+    
     def get_velocity_gradient(t,x,lJHTDB=lJHTDB):
         
         # query the data
         result_grad = lJHTDB.getData(t, x.copy().astype(np.float32), data_set='isotropic1024coarse',sinterp='FD4Lag4', tinterp='PCHIPInt',getFunction='getVelocityGradient')
+        #print(np.shape(result_grad))
+        
+        # put it into the correct shape
+        result_grad_new = np.zeros((len(x),3,3)).astype(np.float32)
+        for i in range(len(x)):
+            this_velgrad = np.array([result_grad[i,0:3],result_grad[i,3:6],result_grad[i,6:]])
+            result_grad_new[i,...] = this_velgrad
+            
+        return result_grad_new
+    
+    def get_velocity_gradient_filtered(t,x,filter_width):
+        
+        # query the data
+        result_grad = lJHTDB.getBoxFilterGradient(t, x.copy().astype(np.float32), data_set='isotropic1024coarse', filter_width=filter_width, field='velocity',)
+        print(np.shape(result_grad))
         
         # put it into the correct shape
         result_grad_new = np.zeros((len(x),3,3)).astype(np.float32)
@@ -69,3 +101,54 @@ if lJHTDB_available:
                 vel_grad[j,i,2] = (u[j,5,i]-u[j,4,i])/(2*delta)
                 
         return vel_grad
+    
+    def get_sphere_kernel(r,n_per_r):
+        x = np.linspace(-r,r,n_per_r*2+1)
+        y = np.linspace(-r,r,n_per_r*2+1)
+        z = np.linspace(-r,r,n_per_r*2+1)
+
+        X,Y,Z = np.meshgrid(x,y,z)
+        XYZ = np.moveaxis(np.array([X,Y,Z]),0,-1)
+        XYZ_flat = np.reshape(XYZ,(np.shape(XYZ)[0]**3,3))
+
+        dist = np.linalg.norm(XYZ_flat,axis=1)
+        XYZ_use = XYZ_flat[dist<=r]
+        return XYZ_use
+    
+    def get_points_in_sphere(center,sphere_kernel):
+        points = np.array([center[i]+sphere_kernel for i in range(len(center))])
+        return points
+    
+    def get_velocity_volavg(t,x,sphere_kernel,lJHTDB=lJHTDB):
+        points = get_points_in_sphere(x,sphere_kernel)
+        #print(np.shape(points))
+        vels = get_velocity(t,points,lJHTDB=lJHTDB) # [bubble,point_in_sphere,axis]
+        #print(np.shape(vels))
+        return np.mean(vels,axis=1)
+    
+    def get_velocity_gradient_volavg(t,x,sphere_kernel,lJHTDB=lJHTDB):
+        points = get_points_in_sphere(x,sphere_kernel)
+        #print('shape of points:')
+        #print(np.shape(points))
+        velgrads = np.array([get_velocity_gradient(t,points[i],lJHTDB=lJHTDB) for i in np.arange(len(x))]) # [bubble,point_in_sphere,axis]
+        #print('shape of velgrads:')
+        #print(np.shape(velgrads))
+        return np.mean(velgrads,axis=1) 
+    
+    def surface_average_of_velocity(t,x,d,filter_size,d2_factor=1.01,avg1=None,lJHTDB=lJHTDB):
+        
+        r = d/2
+        r1 = r
+        r2 = r1*d2_factor
+        #r2 = r1 + dx*2
+        
+        if avg1 is None:
+            avg1 = get_velocity_filtered(t,x,filter_size,lJHTDB=lJHTDB)
+        avg2 = get_velocity_filtered(t,x,filter_size*d2_factor,lJHTDB=lJHTDB)
+        
+        larger = r2**3 * avg2
+        smaller = r1**3 * avg1
+        deriv = (larger-smaller)/(r2-r1)
+        surface_avg = 1./(3*r1**2) * deriv
+        
+        return surface_avg
--- point_bubble_JHTDB/model.py
+++ point_bubble_JHTDB/model.py
@@ -21,6 +21,7 @@ T_eta = 0.0424
 lam = 0.113
 lam_by_Lint = lam / L_int
 
+dx = 2*np.pi / 1024.
 dt = 0.002 # the timestep at which the DNS data is stored, = 10*dt_orig
 dt_orig = 0.0002
 t_max_turbulence = 10
@@ -54,14 +55,21 @@ def calc_lift_force(slip,vort,d,Cl):
     lift = -1 * Cl * np.cross(slip,vort) * (d/2)**3*4./3*np.pi
     return lift
 
-def a_bubble(u,v,velgrad,dudt,d,Cd,Cm,Cl,g,g_dir,pressure_term_coef,lift_term_coef):
+def a_bubble(u,v,velgrad,dudt,d,Cd,Cm,Cl,g,g_dir,pressure_term_coef,lift_term_coef,u_drag=None):
     '''
     calculate a bubble's accceleration given its velocity, the local water
     velocity, and the bubble size
     '''
     
+    if u_drag is None:
+        u_drag = u
+    
     vort = get_vorticity(velgrad)
     slip = v - u
+    if u_drag is not None:
+        slip_drag = v - u_drag
+    else:
+        slip_drag = slip
     
     # pressure force
     press = calc_pressure_force(u,velgrad,dudt,d,Cm) * pressure_term_coef
@@ -70,7 +78,7 @@ def a_bubble(u,v,velgrad,dudt,d,Cd,Cm,Cl,g,g_dir,pressure_term_coef,lift_term_co
     grav = calc_grav_force(g,d,g_dir)
     
     # drag force    
-    drag = calc_drag_force(slip,d,Cd)
+    drag = calc_drag_force(slip_drag,d,Cd)
     
     # lift force
     lift = calc_lift_force(slip,vort,d,Cl) * lift_term_coef
@@ -151,7 +159,7 @@ class PointBubbleSimulation:
         self.n_t = len(self.t)
         
         if fname_save is None:
-            self.fname_save = 'res_beta'+'{:03.2f}'.format(self.beta)+'_A'+'{:06.5f}'.format(self.A)+'_Cm'+'{:03.2f}'.format(self.Cm)+'_Cl'+'{:03.2f}'.format(self.Cl)+'_Cd'+'{:03.2f}'.format(self.Cd)+'_pressureTerm'+'{:04.3f}'.format(self.pressure_term_coef)+'_liftTerm'+'{:04.3f}'.format(self.lift_term_coef)+'.pkl'
+            self.fname_save = 'res_beta'+'{:03.2f}'.format(self.beta)+'_A'+'{:06.5f}'.format(self.A)+'_Cm'+'{:03.2f}'.format(self.Cm)+'_Cl'+'{:03.2f}'.format(self.Cl)+'_Cd'+'{:03.2f}'.format(self.Cd)+'.pkl'
         else:
             self.fname_save = fname_save
             
@@ -179,6 +187,7 @@ class PointBubbleSimulation:
         
         n_t = self.n_t
         n_bubs = self.n_bubs
+        print(n_bubs)
         
         # gravity direction chosen randomly for each bubble
         self.g_dir = np.array([Rotation.random(1).apply([0,0,1]) for _ in range(n_bubs)])[:,0,:] # gravity direction
@@ -221,7 +230,7 @@ class PointBubbleSimulation:
         else:
             print('Did not find file '+str(data_dir+fname_save))
         
-    def _advance(self,ti):
+    def _advance(self,ti,use_filter=False):
         
         t = self.t[ti]
         u = self.u
@@ -229,26 +238,50 @@ class PointBubbleSimulation:
         x = self.x
         dudt = self.dudt
         velgrad = self.velgrad
+        #filter_size = (self.d/2)/np.sqrt(5)
+        #filter_size = (self.d) / (4./3 * np.pi)**(1./3)
+        filter_size = None
         
         # liquid velocity
-        u[ti+1,...] = interface.get_velocity(t,x[ti,...])
+        #print(d,self.d)
+        if use_filter==False:
+            u[ti+1,...] = interface.get_velocity(t,x[ti,...])
+        else:
+            u[ti+1,...] = interface.get_velocity_filtered(t,x[ti,...],filter_size)
         if ti==0:
             u[0,...] = u[1,...]
+            
+        # surface average of liquid velocity
+        #print('getting surface average of the velocity')
+        if use_filter:
+            d2_factor = (filter_size+dx*4)/filter_size
+            u_drag = interface.surface_average_of_velocity(t,x[ti,...],self.d,filter_size,d2_factor=d2_factor,avg1=u[ti+1,...])
+        else:
+            u_drag = None
+        #print('... done')
         
         # time velocity gradient
-        delta = 1e-4
-        u_deltat = interface.get_velocity(t+delta,x[ti,...])
+        
+        if use_filter==False:
+            delta = 1e-4
+            u_deltat = interface.get_velocity(t+delta,x[ti,...])
+        else:
+            delta = 1e-2
+            u_deltat = interface.get_velocity_filtered(t+delta,x[ti,...],filter_size)
         dudt[ti+1,...] = (u_deltat-u[ti+1,...])/delta # future velocity at this point minus current velocity at this point
         
         # liquid velocity gradient
-        velgrad[ti+1,...] = interface.get_velocity_gradient(t,x[ti,...])
+        if use_filter==False:
+            velgrad[ti+1,...] = interface.get_velocity_gradient(t,x[ti,...])
+        else:
+            velgrad[ti+1,...] = interface.get_velocity_gradient_filtered(t,x[ti,...],filter_size)
         
         # start the simulation with v = u + v_q
         if ti==0:
             v[0,...] = u[1,...]*0 + self.g_dir * self.v_q
         
         # bubble acceleration and new velocity
-        a = a_bubble(u[ti+1,...],v[ti,...],velgrad[ti+1,...],dudt[ti+1,...],self.d,self.Cd,self.Cm,self.Cl,self.g,self.g_dir,self.pressure_term_coef,self.lift_term_coef)
+        a = a_bubble(u[ti+1,...],v[ti,...],velgrad[ti+1,...],dudt[ti+1,...],self.d,self.Cd,self.Cm,self.Cl,self.g,self.g_dir,self.pressure_term_coef,self.lift_term_coef,u_drag=u_drag)
         v[ti+1,...] = v[ti,...]+a*self.dt_use
         
         # new position
@@ -302,7 +335,7 @@ def run_model_default_params(changed_params,fname_save=None):
     
     example command to run from a terminal (to run for d=lambda):
     python3 -c "from point_bubble_JHTDB.model import *; beta=0.5; Cd=0.5; Cl=0.25; A=A_given_dByL(lam_by_Lint,beta,Cd); run_model_default_params({'Cl':Cl,'beta':beta,'A':A,'Cd':Cd})"
-    python3 -c "from point_bubble_JHTDB.model import *; beta=0.25; Cd=1; Cl=0.0; A=A_given_dByL(0.12,beta,Cd); run_model_default_params({'Cl':Cl,'beta':beta,'A':A,'Cd':Cd})"
+    python3 -c "from point_bubble_JHTDB.model import *; beta=0.25; Cd=1; Cl=0.25; A=A_given_dByL(0.12,beta,Cd); run_model_default_params({'Cl':Cl,'beta':beta,'A':A,'Cd':Cd,'n_bubs':500})"
     python3 -c "from point_bubble_JHTDB.model import *; beta=0.25; Cd=1; Cl=0.0; A=A_given_dByL(0.2,beta,Cd); run_model_default_params({'Cl':Cl,'beta':beta,'A':A,'Cd':Cd,'n_bubs':500,'dt_factor':0.5})"
     '''
 
@@ -438,3 +471,247 @@ def get_lagrangian_trajectories():
     m.init_sim()
     m.add_data_if_existing()
     m.run_model()
+    
+    
+    
+    
+# class PointBubbleSimulationVolAvg:
+    
+#     def __init__(self,params,fname_save=None):
+        
+#         self.params = params
+        
+#         # bubble parameters
+#         self.beta = params['beta']
+#         self.A = params['A']
+#         self.Cm = params['Cm']
+#         self.Cl = params['Cl']
+#         self.Cd = params['Cd']
+#         self.g = (u_rms**2/L_int)/self.A
+#         self.v_q = u_rms/self.beta
+#         self.d = (3./4) * self.Cd * self.v_q**2 / self.g
+#         self.sphere_kernel = interface.get_sphere_kernel(self.d/2,5)
+        
+#         # either set the pressure, lift scalings to 1 or the value based on the size
+#         if params['scale_by_d']:
+#             self.pressure_term_coef, self.lift_term_coef = size_coefs(self.d)
+#         else:
+#             self.pressure_term_coef = 1 # to be multiplied by the pressure force
+#             self.lift_term_coef = 1 # to be multiplied by the lift force (after accounting for C_L)
+            
+#         # replace the pressure, lift scalings with specified values if they were given
+#         if 'pressure_term_coef' in params:
+#             self.pressure_term_coef = params['pressure_term_coef']
+#         if 'lift_term_coef' in params:
+#             self.lift_term_coef = params['lift_term_coef']            
+                
+#         # simulation parameters
+#         self.n_bubs = params['n_bubs']
+#         self.dt_factor = params['dt_factor']
+#         self.dt_use = self.dt_factor*dt
+        
+#         if 't_min' in list(params.keys()):
+#             self.t_min = params['t_min']
+#         else:
+#             self.t_min = 0
+        
+#         if 't_max' in list(params.keys()):
+#             self.t_max = params['t_max']
+#             print('set specified t_max')
+#             print(params['t_max'],self.t_max)
+#         else:
+#             self.t_max = t_max_turbulence
+#             print('now setting to t_max_turbulence')
+            
+#         print(self.t_min,self.t_max)
+#         self.t = np.arange(self.t_min,self.t_max,self.dt_use)
+#         self.n_t = len(self.t)
+#         print(self.n_t)
+        
+#         if fname_save is None:
+#             self.fname_save = 'res_beta'+'{:03.2f}'.format(self.beta)+'_A'+'{:06.5f}'.format(self.A)+'_Cm'+'{:03.2f}'.format(self.Cm)+'_Cl'+'{:03.2f}'.format(self.Cl)+'_Cd'+'{:03.2f}'.format(self.Cd)+'_pressureTerm'+'{:04.3f}'.format(self.pressure_term_coef)+'_liftTerm'+'{:04.3f}'.format(self.lift_term_coef)+'_volavg'+'.pkl'
+#         else:
+#             self.fname_save = fname_save
+            
+#     def save(self,fname_save=None):
+#         if fname_save is None:
+#             fname_save = self.fname_save
+        
+#         save_vars = ['beta','A',
+#                      'Cm','Cl','Cd',
+#                      'pressure_term_coef','lift_term_coef',
+#                      'g','v_q','d','g_dir',
+#                      'n_bubs','dt_factor','dt_use','t','n_t',
+#                      'x','v','u','dudt','velgrad','ti']
+        
+#         res = {attr:getattr(self,attr) for attr in save_vars}
+#         print('Saving data to '+fname_save)
+#         with open(data_dir+fname_save, 'wb') as handle:
+#             pickle.dump(res, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        
+        
+#     def init_sim(self):
+#         '''
+#         Initialize the simulation
+#         '''
+        
+#         n_t = self.n_t
+#         n_bubs = self.n_bubs
+        
+#         # gravity direction chosen randomly for each bubble
+#         self.g_dir = np.array([Rotation.random(1).apply([0,0,1]) for _ in range(n_bubs)])[:,0,:] # gravity direction
+#         #self.g_dir = np.array([np.array([0,0,1]) for _ in range(n_bubs)])#[:,0,:] # gravity direction
+        
+#         self.x = np.zeros((n_t,n_bubs,3))
+#         self.u = np.zeros((n_t,n_bubs,3))
+#         self.v = np.zeros((n_t,n_bubs,3))
+#         self.velgrad = np.zeros((n_t,n_bubs,3,3))
+#         self.dudt = np.zeros((n_t,n_bubs,3))
+        
+#         self.x[0,...] = np.random.uniform(low=0,high=2*np.pi,size=(n_bubs,3))
+                
+#         self.ti = 0
+#         #self.t = np.arange(0,self.t_max,self.dt_use)
+#         #self.n_t = len(self.t)
+        
+#     def add_data(self,res):
+#         '''
+#         Add partially-complete simulation data stored in the dict res
+#         '''
+        
+#         self.x = res['x']
+#         self.u = res['u']
+#         self.v = res['v']
+#         self.velgrad = res['velgrad']
+#         self.dudt = res['dudt']
+#         self.ti = res['ti']-1
+#         self.g_dir = res['g_dir']
+        
+#     def add_data_if_existing(self,fname_save=None):
+#         if fname_save is None:
+#             fname_save = self.fname_save
+            
+#         if os.path.isfile(data_dir+fname_save):
+#             print('Loading data from '+data_dir+fname_save)
+#             with open(data_dir+fname_save, 'rb') as handle:
+#                 res = pickle.load(handle)
+#             self.add_data(res)
+#         else:
+#             print('Did not find file '+str(data_dir+fname_save))
+        
+#     def _advance(self,ti,use_filter=False):
+        
+#         t = self.t[ti]
+#         u = self.u
+#         v = self.v
+#         x = self.x
+#         dudt = self.dudt
+#         velgrad = self.velgrad
+        
+#         # liquid velocity
+#         u[ti+1,...] = interface.get_velocity_volavg(t,x[ti,...],self.sphere_kernel)
+#         if ti==0:
+#             u[0,...] = u[1,...]
+        
+#         # time velocity gradient
+#         delta = 1e-4
+#         u_deltat = interface.get_velocity_volavg(t+delta,x[ti,...],self.sphere_kernel)
+#         dudt[ti+1,...] = (u_deltat-u[ti+1,...])/delta # future velocity at this point minus current velocity at this point
+        
+#         # liquid velocity gradient
+#         velgrad[ti+1,...] = interface.get_velocity_gradient_volavg(t,x[ti,...],self.sphere_kernel)
+#         #for bi in np.arange(self.n_bubs):
+#         #    velgrad[ti+1,bi,...] = interface.get_velocity_gradient_volavg(t,x[ti,bi,...],self.sphere_kernel)
+        
+#         # start the simulation with v = u + v_q
+#         if ti==0:
+#             v[0,...] = u[1,...]*0 + self.g_dir * self.v_q
+        
+#         # bubble acceleration and new velocity
+#         a = a_bubble(u[ti+1,...],v[ti,...],velgrad[ti+1,...],dudt[ti+1,...],self.d,self.Cd,self.Cm,self.Cl,self.g,self.g_dir,self.pressure_term_coef,self.lift_term_coef)
+#         v[ti+1,...] = v[ti,...]+a*self.dt_use
+        
+#         # new position
+#         x[ti+1,...] = x[ti,...]+v[ti+1,...]*self.dt_use
+        
+#         # store the data
+#         self.u = u
+#         self.v = v
+#         self.x = x
+#         self.dudt = dudt
+#         self.velgrad = velgrad
+        
+#     def run_model(self,save_every=500):
+        
+#         print('running the model')
+        
+#         while self.ti < self.n_t-1:            
+            
+#             # advance the simulation
+#             t_start = time_pkg.time()
+#             self._advance(self.ti)
+#             print(self.fname_save+' : Timestep '+str(self.ti)+', time '+'{:06.4f}'.format(self.t[self.ti])+', took '+'{:01.4f}'.format(time_pkg.time()-t_start)+' s.')
+#             self.ti = self.ti + 1
+            
+#             # save, if necessary
+#             if (self.ti % save_every) == 0:
+#                 self.save()
+                
+#         self.save()
+        
+        
+# def run_volavg_sim(beta,d_star,fname_save=None):
+    
+#     '''
+#     python3 -c "from point_bubble_JHTDB.model import run_volavg_sim; run_volavg_sim(0.5,0.12)"
+#     '''
+    
+#     params = default_params 
+    
+#     Cd = 1
+#     A = A_given_dByL(d_star,beta,Cd)
+#     params['beta'] = beta
+#     params['A'] = A
+#     params['Cd'] = Cd
+#     params['Cl'] = 0
+#     params['n_bubs'] = 8
+#     p = PointBubbleSimulationVolAvg(params,fname_save=fname_save)
+#     p.init_sim()
+#     p.add_data_if_existing()
+#     p.run_model()
+    
+    
+def run_filt_sim(beta,d_star,fname_save=None,Fr=None):
+    
+    '''
+    python3 -c "from point_bubble_JHTDB.model import run_filt_sim; run_filt_sim(1,0.12,fname_save=None)"
+    python3 -c "from point_bubble_JHTDB.model import run_filt_sim; run_filt_sim(1,None,Fr=0.2)"
+    '''
+    
+    if fname_save is None and d_star is not None:
+        fname_save = 'filt_beta'+'{:01.2f}'.format(beta)+'_dstar'+'{:01.3f}'.format(d_star)+'_filtSizeForSphereVol.pkl'
+    
+    params = default_params 
+    
+    Cd = 1
+    
+    if Fr is None:
+        A = A_given_dByL(d_star,beta,Cd)
+        
+    else:
+        A = Fr**2
+        fname_save = 'filt_beta'+'{:01.2f}'.format(beta)+'_Fr'+'{:01.3f}'.format(Fr)+'_filtSizeForSphereVol.pkl'
+    #print(beta)
+    #print(d_star)
+    #else:
+        #A = A
+        #d_star = 
+    params['beta'] = beta
+    params['A'] = A
+    params['Cd'] = Cd
+    params['Cl'] = 0
+    params['n_bubs'] = 500
+    p = PointBubbleSimulation(params,fname_save=fname_save)
+    p.init_sim()
+    p.add_data_if_existing()
+    p.run_model()
