50b53b3d6853a376fe5d5f583f39103a6a834d7b
Dan Ruth <danjruth@gmail.com>
2021-07-05 11:26:38 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(name='pointparticlesinaflow',
+      version='0.1',
       packages=find_packages(),
-      install_requires=[],
+      author='Daniel J. Ruth',
+      url=r'https://github.com/DeikeLab/point-particles-in-a-flow/',
+      description='Simulation of point-particles in flow fields',
+      long_description=open('README.md').read(),
+      install_requires=[
+          'numpy',
+          'pandas',
+          'scipy',
+          ],
       )
